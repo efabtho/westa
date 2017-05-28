9f45fc51acc3f6197f917fbcd79bde3c8a9d97f3
Thomas <thomas.fabian@web.de>
2017-05-28 14:13:37 +0200

    exception handling optimized for mailWeatherReportOnRequest.py. hope for more stability when polling for new 'get wr' commands in mailbox.

--- src/mailWeatherReportOnRequest.py
+++ src/mailWeatherReportOnRequest.py
@@ -15,7 +15,7 @@ import ConfigParser
 
 from mailWeatherReport import mailWeatherReport
 
-DEBUG = False
+DEBUG = True
 
 # RPi3 weather station dedicated email account
 config = ConfigParser.ConfigParser()
@@ -37,45 +37,50 @@ def parse_list_response(line):
 
 try:
     if DEBUG:
-        print "called at: ",strftime("%d-%m-%Y %H:%M:%S", localtime())
+        print "called at: ", strftime("%d-%m-%Y %H:%M:%S", localtime())
         
     m = imaplib.IMAP4_SSL(Hostname)
     m.login(Username, Password)
 
-    typ, mailbox_data = m.list(Mailbox)
+except:
+    print "Error occured! Could not login..."
+    quit()
     
-    for line in mailbox_data:
-        flags, delimiter, mailbox_name = parse_list_response(line)
-        m.select(mailbox_name, readonly=False)
-        typ, [msg_ids] = m.search(None, '(FROM '+Sender+' SUBJECT '+CmdSubject+' UNSEEN)')
-        # convert str -> list (to enable to sum up the no of found emails)
-        msg_ids_list = msg_ids.split()
+typ, mailbox_data = m.list(Mailbox)
 
-        if DEBUG:
-            print "no of email ids with matched request criterias: ", len(msg_ids_list)
-            for id in msg_ids_list:
-                print "id = ",id
+for line in mailbox_data:
+    flags, delimiter, mailbox_name = parse_list_response(line)
+    m.select(mailbox_name, readonly=False)
+    typ, [msg_ids] = m.search(None, '(FROM '+Sender+' SUBJECT '+CmdSubject+' UNSEEN)')
+    # convert str -> list (to enable to sum up the no of found emails)
+    msg_ids_list = msg_ids.split()
 
-        # check if at least one email matches the request criteria
-        if len(msg_ids_list) >= 1:
-            #weather report was requested, send it...
-            mailWeatherReport()
-            if DEBUG:
-                print "valid request detected: send weather report"
-            #delete request message(s)
-            msg_ids = ','.join(msg_ids.split(' ')) # comma sep. list of ids needed
-            if DEBUG:
-                print "msg_ids =", msg_ids
-            typ, response = m.store(msg_ids, '+FLAGS', r'(\Deleted)')
-            if DEBUG:
-                print "response = ", response
-        else:
-            if DEBUG:
-                print "no valid email request detected"
-
-finally:
-    m.close()
-    m.logout()
     if DEBUG:
-        print "Logout"
+        print "no of email ids with matched request criterias: ", len(msg_ids_list)
+        for id in msg_ids_list:
+            print "id = ",id
+
+    # check if at least one email matches the request criteria
+    if len(msg_ids_list) >= 1:
+        #weather report was requested, send it...
+        ###################
+        mailWeatherReport()
+        ###################
+        if DEBUG:
+            print "valid request detected: send weather report"
+        #delete request message(s)
+        msg_ids = ','.join(msg_ids.split(' ')) # comma sep. list of ids needed
+        if DEBUG:
+            print "msg_ids =", msg_ids
+        typ, response = m.store(msg_ids, '+FLAGS', r'(\Deleted)')
+        if DEBUG:
+            print "response = ", response
+    else:
+        if DEBUG:
+            print "no valid email request detected"
+
+m.close()
+m.logout()
+if DEBUG:
+    print "Logout"
 
